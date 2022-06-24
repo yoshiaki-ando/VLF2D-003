5ac7a2afe9ef0e48ca5fd050cd88ab4b27b87477
ANDO Yoshiaki <y-ando@uec.ac.jp>
2022-06-24 11:06:44 +0900

    debug: 削除した変数が残っており、未定義変数となっていた

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv){
   ///時間ループ///
   for(int n = 1; n <= Nt; n++){
     if ( n%100 == 0 ){
-      std::cout << n << " / " << Nt << " : " << Lp/1000 << "\n";
+      std::cout << n << " / " << Nt << "\n";
     }
     
     int NEW = (n+1) % 2;
